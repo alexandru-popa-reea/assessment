2c382e73bd25019b639a1b0cd988d43b177d4563
Alexandru Popa <alexandru.popa@reea.net>
2021-11-22 13:02:38 +0200

    Added constraint to REL_USERS_TO_PATIENTS

--- src/main/resources/db/migration/V03__Create_rel_users_to_patients_table.sql
+++ src/main/resources/db/migration/V03__Create_rel_users_to_patients_table.sql
@@ -1,5 +1,9 @@
 create table REL_USERS_TO_PATIENTS
 (
     USER_ID    bigint not null,
-    PATIENT_ID bigint not null
+    PATIENT_ID bigint not null,
+    constraint FK__REL_USERS_TO_PATIENTS__TO_PATIENTS
+        foreign key (PATIENT_ID) references PATIENTS (ID),
+    constraint FK__REL_USERS_TO_PATIENTS__TO_USERS
+        foreign key (USER_ID) references USERS (ID)
 );
